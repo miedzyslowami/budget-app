6e65f1b63ee626be087abe2d658412298f0fb786
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2019-07-16 13:44:19 +0200

    Updates

---
+++ precache-manifest.4bcffccc2714affe44c538576c50b5ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1b1d9fff3754edcfa95020db3e21b3",
+    "url": "/budget-app/index.html"
+  },
+  {
+    "revision": "c7b00168534cd82d1df3",
+    "url": "/budget-app/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "ca53fa19b5c198d02503",
+    "url": "/budget-app/static/js/2.148bfd78.chunk.js"
+  },
+  {
+    "revision": "c7b00168534cd82d1df3",
+    "url": "/budget-app/static/js/main.e9e4296e.chunk.js"
+  },
+  {
+    "revision": "5fadb49a05f3b19c1827",
+    "url": "/budget-app/static/js/runtime~main.bfda720e.js"
+  }
+]);
