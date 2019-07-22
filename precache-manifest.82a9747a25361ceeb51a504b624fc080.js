009075a3783c883892f0b11353fa7eb3dde737cd
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2019-07-22 13:34:07 +0200

    Updates

--- precache-manifest.82a9747a25361ceeb51a504b624fc080.js
+++ precache-manifest.82a9747a25361ceeb51a504b624fc080.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7a784003ecdd489d662969b9044b019",
+    "revision": "22b05d6465a59846917a6f727d451067",
     "url": "/budget-app/index.html"
   },
   {
-    "revision": "67c6170865d28d098a52",
+    "revision": "045d55fa0cddb69f0d59",
     "url": "/budget-app/static/css/main.8ef63b32.chunk.css"
   },
   {
-    "revision": "ca53fa19b5c198d02503",
-    "url": "/budget-app/static/js/2.148bfd78.chunk.js"
+    "revision": "3759ebe3c5dbb8809f46",
+    "url": "/budget-app/static/js/2.18511857.chunk.js"
   },
   {
-    "revision": "67c6170865d28d098a52",
-    "url": "/budget-app/static/js/main.272dbd2d.chunk.js"
+    "revision": "045d55fa0cddb69f0d59",
+    "url": "/budget-app/static/js/main.221a108c.chunk.js"
   },
   {
     "revision": "5fadb49a05f3b19c1827",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/budget-app/precache-manifest.69fc7a56b76f76aae49b7c0ce72768d0.js"
+  "/budget-app/precache-manifest.82a9747a25361ceeb51a504b624fc080.js"
 );
 
 self.addEventListener('message', (event) => {
